010dc06f2f6b838d5fd73c5cdd4cf0ce99cd0075
ujj-kumar <batman.c731@gmail.com>
2025-03-06 02:17:26 +0530

    Package Updated

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.4"
+__version__ = "0.0.3"
 REPO_NAME = "MongoDB-PyPI"
-PKG_NAME= "MongoDB-PyPI" # By this name, package will be displayed on PyPI
+PKG_NAME= "mongodb_pypi" # By this name, package will be displayed on PyPI
 AUTHOR_USER_NAME = "Ujjwal Kumar"
 AUTHOR_EMAIL = "batman.c137@gmail.com"
 
