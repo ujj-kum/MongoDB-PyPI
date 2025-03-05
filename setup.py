1b610941e2d0d7039ea7f3cc6921b5001cb25289
ujj-kumar <batman.c731@gmail.com>
2025-03-06 02:09:09 +0530

    Package Updated

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from typing import List
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()     
+   
+
+__version__ = "0.0.4"
+REPO_NAME = "MongoDB-PyPI"
+PKG_NAME= "MongoDB-PyPI" # By this name, package will be displayed on PyPI
+AUTHOR_USER_NAME = "Ujjwal Kumar"
+AUTHOR_EMAIL = "batman.c137@gmail.com"
+
+setup(
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with database.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    )
